b03d01524652cbea7159208e26c5e78c94767c3e
Nils Domin <nils@blackforestcode.com>
2019-06-09 13:49:48 +0200

    Refactored UserItem and Navbar to functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt={`Avatar from ${login}`}
-          className="round-img"
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a
-            href={html_url}
-            className="btn btn-dark btn-sm my-1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            More...
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt={`Avatar from ${login}`}
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          className="btn btn-dark btn-sm my-1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          More...
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
