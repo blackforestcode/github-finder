f4ef083f1236facf8cb2a45bcedde67820b32ab1
Nils Domin <nils@blackforestcode.com>
2019-06-10 13:16:18 +0200

    Added links to navbar and about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 
+// Layout
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+
+// Users
 import Users from './components/users/Users';
 import Search from './components/users/Search';
-import Alert from './components/layout/Alert';
+
+// Pages
+import About from './components/pages/About';
 
 class App extends Component {
   state = {
@@ -47,19 +54,32 @@ class App extends Component {
   render() {
     const { loading, users } = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const About = () => (
+  <Fragment>
+    <h1>About this App</h1>
+    <p>An App to search GitHub users.</p>
+    <p>Version: 1.0.0</p>
+  </Fragment>
+);
+
+export default About;
