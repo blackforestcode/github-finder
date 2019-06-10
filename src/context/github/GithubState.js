83af7c2629d8e43a7045005beec341e3e87a3845
Nils Domin <nils@blackforestcode.com>
2019-06-10 19:50:59 +0200

    Changed a small thing

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,17 +10,6 @@ import {
   GET_REPOS
 } from '../types';
 
-let githubClientId;
-let githubClientSecret;
-
-if (process.env.NODE_ENV !== 'production') {
-  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
-} else {
-  githubClientId = process.env.GITHUB_CLIENT_ID;
-  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-}
-
 const GithubState = props => {
   const initialState = {
     users: [],
@@ -35,7 +24,9 @@ const GithubState = props => {
   const searchUsers = async text => {
     setLoading(); // -> calls setLoading function below!
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     dispatch({
@@ -48,7 +39,9 @@ const GithubState = props => {
   const getUser = async username => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     dispatch({
@@ -61,7 +54,9 @@ const GithubState = props => {
   const getUserRepos = async username => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     dispatch({
